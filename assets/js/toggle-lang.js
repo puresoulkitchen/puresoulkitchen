a1b78ab75b23a2997828f7940eff95552266dddf
puresoulkitchen <puresoulkitchen471@gmail.com>
2025-06-18 22:09:24 +0800

    Update toggle-lang.js

--- assets/js/toggle-lang.js
+++ assets/js/toggle-lang.js
@@ -1,8 +1,3 @@
 function toggleLanguage() {
-  const current = window.location.pathname;
-  if (current.includes('/zh/')) {
-    window.location.href = current.replace('/zh/', '/');
-  } else {
-    window.location.href = '/zh' + current;
-  }
+  alert("Language toggle function is not yet implemented.");
 }
